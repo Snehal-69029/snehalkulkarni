417fa7a12642314a351b5a11aa9765200342efa6
Snehal-69029 <108972540+Snehal-69029@users.noreply.github.com>
2022-07-11 16:56:29 +0530

    Create DAY1_PROGRAM_3.C

---
+++ DAY1/DAY1_PROGRAM_3.C
@@ -0,0 +1,15 @@
+/*3) The entire area of a circular garden is to be covered with grass by a Gardener. 
+The radius of the garden is given by the user input. Calculate the area that has to be covered.*/
+
+#include <stdio.h>
+
+int main()
+{
+    float r,b;
+    printf("enter the radius of garden\n");   //take radius value from user
+    scanf("%f",&r);
+    b=3.14*r*r;                               // area of circle formula 
+    printf("area of circle garden is =%f",a); //print covered area
+    return 0;
+    
+}
