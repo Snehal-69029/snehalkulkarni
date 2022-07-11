2ea375c24233fce8c05e46a9f7aacaaf888d2179
Snehal-69029 <108972540+Snehal-69029@users.noreply.github.com>
2022-07-11 17:46:24 +0530

    Create DAY4_PROGRAM_4.C

---
+++ DAY4/DAY4_PROGRAM_4.C
@@ -0,0 +1,11 @@
+//2)EVEN AND ODD NUMBER USING CONDITIONAL OPERATOR
+
+#include <stdio.h>
+int main()
+{
+int num;
+scanf("%d",&num);
+(num%2==0)?printf("even number"):printf("odd number");       //if num%2==0 then print even otherwise print odd
+
+return 0;
+}
