d91bd9dc6dfe9823b68cf914e1fb9a2d89355e58
Snehal-69029 <108972540+Snehal-69029@users.noreply.github.com>
2022-07-11 17:32:40 +0530

    Create DAY3_PROGRAM_6.C

---
+++ DAY3/DAY3_PROGRAM_6.C
@@ -0,0 +1,8 @@
+//6.Write program to accept hex value as i/p and right shift bit value also as i/p and print the value after doing the hex right shift
+
+#include <stdio.h>
+int main()
+{
+    int b=0xEF>>3;        //accept hex value as i/p and right shift bit value              
+    printf("%x",b);
+}
