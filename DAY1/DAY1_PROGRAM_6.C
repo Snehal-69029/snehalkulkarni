c3bc3f81702d7bb5eee0e148c5a2c6ce31977613
Snehal-69029 <108972540+Snehal-69029@users.noreply.github.com>
2022-07-11 17:00:07 +0530

    Create DAY1_PROGRAM_6.C

---
+++ DAY1/DAY1_PROGRAM_6.C
@@ -0,0 +1,11 @@
+//6)Consider a character input and find its ASCII value. 
+
+#include <stdio.h>
+
+int main()
+{
+   char ch='C';           //assign chara 
+   printf("%d\n",ch);     //chara return ascii value %d
+   
+  
+}
