351a5c2051524d8f42a4af537d4be44997cccb30
Snehal-69029 <108972540+Snehal-69029@users.noreply.github.com>
2022-07-11 17:47:28 +0530

    Create DAY4_PROGRAM_5.C

---
+++ DAY4/DAY4_PROGRAM_5.C
@@ -0,0 +1,11 @@
+//3)POSITIVE OR NEGATIVE NUMBER USING CONDITIONAL OPERATOR
+
+#include <stdio.h>
+int main()
+{
+int num;
+scanf("%d",&num);
+(num>0)?printf("positive number"):printf("negative number");  // if num>0 then print +ve num otherwise print -ve num
+
+return 0;
+}
