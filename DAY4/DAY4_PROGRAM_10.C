da6105b37f2773c55c8f89feca112e3df48535ab
Snehal-69029 <108972540+Snehal-69029@users.noreply.github.com>
2022-07-11 17:52:55 +0530

    Create DAY4_PROGRAM_10.C

---
+++ DAY4/DAY4_PROGRAM_10.C
@@ -0,0 +1,21 @@
+/*(J)COMMA OPERATOR
+
+(K) SIZEOF OPERATOR 
+
+(1) FINDOUT SIZE FOR ALL DATATYPES AND VARIABLES */
+
+#include <stdio.h>
+
+int main()
+{
+int a;
+float b;
+char c;
+double d;
+printf("size of int: %d bytes \n",sizeof(a));     //size of int
+printf("size of float: %d bytes \n",sizeof(b));   //size of float
+printf("size of char: %d bytes \n",sizeof(c));    //size of char
+printf("size of double: %d bytes \n",sizeof(d));  //size of double
+return 0;
+
+}
